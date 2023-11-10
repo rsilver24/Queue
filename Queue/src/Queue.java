04d48c7410eadea1d7bfc4c3704d23375c66325d
rsilver24 <rsilver24@kentdenver.org>
2023-11-10 11:49:14 -0700

    Constructors, Methods, and Instance Data completed w/ new QueueNode class

--- Queue/src/Queue.java
+++ Queue/src/Queue.java
@@ -1,3 +1,53 @@
-public class Queue {
-    //Code !!
+public class Queue<E> {
+    //Instance Data
+
+    private QueueNode first;
+    private int size;
+
+
+    //Constructors
+
+    public Queue(){
+        size = 0;
+        first = new QueueNode();
+    }
+
+
+    //Methods
+
+    void enqueue(E element){
+        QueueNode newNode = new QueueNode(element);
+        QueueNode subNode = first;
+        if (size == 0){
+            first.setValue(element);
+        }else if (size == 1){
+            first.setChildNode(newNode);
+        }else{
+            for (int i = 1; i < size; i++){
+                subNode = subNode.getChildNode();
+            } subNode.setChildNode(newNode);
+        } size++;
+    }
+
+    E dequeue(){
+        E value = (E)first.getValue();
+        first = first.getChildNode();
+        size--;
+        return value;
+    }
+
+    boolean isEmpty(){
+        if(size == 0)
+            return true;
+        else
+            return false;
+    }
+
+    int size(){
+        return size;
+    }
+
+    E peek(){
+        return (E)first.getValue();
+    }
 }
---
+++ Queue/src/QueueNode.java
@@ -0,0 +1,61 @@
+// Roan Silver      Created: September 21st 2023        Last Edits: November 10th, 2023
+// Collaborators: Luke O'Drobinak & Ethan Ayers       Peer Comments: September 4th, 2023
+public class QueueNode<T, E> {
+
+    // Instance Data !!!
+
+    private QueueNode childQueueNode;
+    private T value;
+
+    // CONSTRUCTORS :D
+
+    // Default "Empty" Constructor
+    public QueueNode(){
+        childQueueNode = null;
+        value = null;
+    }
+
+    // 1 Input Constructor
+    public QueueNode(T valueInput){
+        this.value = valueInput;
+        childQueueNode = null;
+    }
+
+    // 2 Input Constructor
+    public QueueNode(T valueInput, QueueNode childQueueNodeInput){
+        this.value = valueInput;
+        this.childQueueNode = childQueueNodeInput;
+    }
+
+    // ACCESSORS :)
+
+    public QueueNode getChildNode(){
+        return childQueueNode;
+    }
+
+    public T getValue(){
+        return value;
+    }
+
+    // MUTATORS :/
+
+    public void setChildNode(QueueNode newChildQueueNode){
+        childQueueNode = newChildQueueNode;
+    }
+
+    public void setValue(T newValue){
+        value = newValue;
+    }
+
+    // Method Overrides >:(
+
+    public String toString(){
+        return "Node: " + value;
+    }
+
+    public boolean equals(QueueNode passingQueueNode){
+        if (this.getValue().equals(passingQueueNode.getValue())){
+            return true;
+        } return false;
+    }
+}
